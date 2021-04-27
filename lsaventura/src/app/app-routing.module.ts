9aa2c8f50e178e2407184792b19e93805d22877f
= <=>
2021-04-27 16:59:00 +0200

    adding some last changes of merge mistakes, now its fixed

--- lsaventura/src/app/app-routing.module.ts
+++ lsaventura/src/app/app-routing.module.ts
@@ -1,27 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { CreateActivityComponent } from './activities/components/create-activity/create-activity.component';
 import { ListActivityComponent } from './activities/components/list-activity/list-activity.component';
-
-<<<<<<< HEAD
-import { Redirect404Component } from './auth/components/redirect404/redirect404.component';
-=======
 import { DeleteActivityComponent } from './activities/components/delete-activity/delete-activity.component';
 import { UpdateActivityComponent } from './activities/components/update-activity/update-activity.component';
 
-import { Redirect404Component } from './redirect404/redirect404.component';
->>>>>>> dev2
+import { Redirect404Component } from '../app/redirect404/redirect404.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: AppComponent },
+  { path: 'create', component: CreateActivityComponent },
+  { path: 'list', component: ListActivityComponent },
   { path: 'delete', component: DeleteActivityComponent },
   { path: 'update', component: UpdateActivityComponent },
   { path: '**', component: Redirect404Component },
-=======
-const routes: Routes = [
-  {path: 'create', component:CreateActivityComponent},
-  {path: 'list', component:ListActivityComponent}
->>>>>>> dev1
 ];
 
 @NgModule({
