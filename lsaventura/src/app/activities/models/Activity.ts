e5a6af34591509c03cb643eca441704859bf8ae8
= <=>
2021-04-27 16:09:58 +0200

    pues nada, aqui estamos resolviendo el conflicto de merge en el archivo de activity del modelo activity

--- lsaventura/src/app/activities/models/Activity.ts
+++ lsaventura/src/app/activities/models/Activity.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-export interface Activity{
-    nombre:string,
-    fecha:Date,
-    prediccion:string
-=======
 export interface Activity {
-    nombre: String,
-    dia: Date,
-    prediccion: String,
->>>>>>> dev2
+    nombre: string,
+    fecha: Date,
+    prediccion: string
 }
