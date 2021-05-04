820346db16552e808076d1380faf1528e16cd6b4
= <=>
2021-05-04 16:19:38 +0200

    la cagué, pero rectifiqué y los unicos cambios que dejo en el main son los paquetes instalados de bootstrap y poco mas

--- lsaventura/src/app/app.module.ts
+++ lsaventura/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrModule } from 'ngx-toastr';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
@@ -25,7 +26,8 @@ import { ActivitiesModule } from './activities/activities.module';
     AngularFirestoreModule,
     ActivitiesModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
