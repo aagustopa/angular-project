80100132e58c610a3705b359d525478cf477cebc
= <=>
2021-05-13 13:48:42 +0200

    hosting de firebase

--- lsaventura/src/environments/environment.prod.ts
+++ lsaventura/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBwv7Xp5D-djwovf-950sYJYDuuL_i4bkw",
+    authDomain: "lsaventura-c4bd7.firebaseapp.com",
+    projectId: "lsaventura-c4bd7",
+    storageBucket: "lsaventura-c4bd7.appspot.com",
+    messagingSenderId: "844052763074",
+    appId: "1:844052763074:web:907147dc3508d1ee7fc65b",
+    measurementId: "G-0KVWTVM3EE"
+  }
 };
